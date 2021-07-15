3b9d6ae170c9b13a96b82a3d49abfb7ed8e12bbf
Alberto Gonzalez <78628287+dalismo@users.noreply.github.com>
2021-07-15 14:28:27 -0400

    creating otu_id list

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 function init() {
     d3.json("samples.json").then(function (jsonData) {
         let data = jsonData;
-        console.log(data);
+        // console.log(data);
 
         //Getting the name ID
         let dataNames = data.names;
@@ -13,7 +13,27 @@ function init() {
         // First ID variable
         let sampleID = "940";
 
-        datapull(sampleID);
+        retrieve(sampleID);
     });
 }
+
+function retrieve(sampleID) {
+    d3.json("samples.json").then(function (jsonData) {
+      console.log("1. retrieve data");
+      let data = jsonData;
+  
+      let testSample = data.samples.filter((val) => val.id == sampleID);
+      //console.log(testSample;
+
+      let testSampleObj = testSample[0];
+      //console.log(testSampleObj);
+  
+      let otu_ids = testSampleObj.otu_ids;
+      //console.log(otu_ids);
+  
+      let otu_idList = [];
+      for (let i = 0; i < otu_ids.length; i++) {
+        otu_idList.push(`OTU# ${otu_ids[i]}`);
+      }
+  
 
